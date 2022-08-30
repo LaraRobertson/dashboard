940f263b8bf23ee76b2095a362a57597fdb7c70b
LaraRobertson <lara@lararobertson.com>
2022-08-30 10:12:01 -0400

    Updates

---
+++ precache-manifest.1a6a4965a5ab2443f4c97b05f55a952a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a31dba8fec70e97e71630dcb9bd15d0",
+    "url": "/LaraRobertson/dashboard/index.html"
+  },
+  {
+    "revision": "0cdf1e341073839149f3",
+    "url": "/LaraRobertson/dashboard/static/css/main.0c8a39e2.chunk.css"
+  },
+  {
+    "revision": "ed2e4828c5998a88aedc",
+    "url": "/LaraRobertson/dashboard/static/js/2.2c509717.chunk.js"
+  },
+  {
+    "revision": "e66d5f62cddbd9f14d019408603ab562",
+    "url": "/LaraRobertson/dashboard/static/js/2.2c509717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cdf1e341073839149f3",
+    "url": "/LaraRobertson/dashboard/static/js/main.cc7d3548.chunk.js"
+  },
+  {
+    "revision": "9b25ded1555054fbcc09",
+    "url": "/LaraRobertson/dashboard/static/js/runtime-main.c5b8720c.js"
+  }
+]);
